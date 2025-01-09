bb77826c551ea799e8c5b85f9d525466c6fe2b8b
Nagendra Sriram <134068568+Nagendrasriram@users.noreply.github.com>
2025-01-09 21:24:18 +0530

    Update Number1.java

--- Number1.java
+++ Number1.java
@@ -1,3 +1,4 @@
+//Write a program to print whether a number is even or odd, also take input from the user.
 import java.util.Scanner;
 public class Number1{
 	public static void main(String[] args) {
