3c4c6ee2ddc75f90399a0cf521753a298f899c93
Nagendrasriram <nagendra3feb@gmail.com>
2025-01-09 21:22:57 +0530

    Resolved merge conflicts

---
+++ Number1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Number1{
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n = sc.nextInt();
+        if(n%2 == 0)
+        {
+            System.out.println("The number is even");
+        }
+        else{
+            System.out.println("The number is odd");
+        }
+    }
+}
