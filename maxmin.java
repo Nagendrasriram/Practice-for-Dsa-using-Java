9fb8947aedab8eea8952661c61c7b1cbd765d25a
Nagendrasriram <nagendra3feb@gmail.com>
2025-01-11 21:41:37 +0530

    commit it

--- maxmin.java
+++ maxmin.java
@@ -1,4 +1,3 @@
-//Define two methods to print the maximum and the minimum number respectively among three numbers entered by the user.
 import java.util.Scanner;
 public class maxmin {
     public static void main(String[] args) {
@@ -9,31 +8,41 @@ public class maxmin {
         int b=sc.nextInt();
         System.out.print("Enter the third number: ");
         int c=sc.nextInt();
-        int max, min;
-        max = max(a, b, c);
-        min = min(a, b, c);
-        System.out.println("Maximum: " + max);
-        System.out.println("Minimum: " + min);
+        int max,min;
+        max = max (a, b,c);
+        min = min (a, b,c);
+        System.out.println("Max:"+max);
+        System.out.println("Min:"+min);
     }
-
-    static int max(int a, int b, int c) {
-        if (a > b && a > c) {
-            return a;
-        } else if (b > a && b > c) {
-            return b;
-        } else {
-            return c;
+        static int max(int a, int b, int c)
+     {
+            if(a>b && a>c)
+            {
+               return a;
+            }
+            else if(b>a && b>c)
+            {
+                return b;
+                }
+                else
+                {
+                    return c;
+                    }
         }
-    }
-
-    static int min(int a, int b, int c) {
-        if (a < b && a < c) {
-            return a;
-        } else if (b < c && b < a) {
-            return b;
-        } else {
-            return c;
+        static int min(int a,int b,int c)
+        {
+            if(a<b && a<c)
+            {
+                return a;
+            }
+            else if(b<c && b<a)
+            {
+                return b;
+            }
+            else
+            {
+                return c;
+            }
         }
-    }
     }    
 
