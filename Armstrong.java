e76ba1f41beb57df6d04d18dbba73bb0ac132230
Nagendrasriram <nagendra3feb@gmail.com>
2025-02-14 19:17:36 +0530

    Armstrong number

---
+++ Armstrong.java
@@ -0,0 +1,33 @@
+// Armstrong Numbers
+// Difficulty: EasyAccuracy: 49.88%Submissions: 170K+Points: 2
+// You are given a 3-digit number n, Find whether it is an Armstrong number or not.
+
+// An Armstrong number of three digits is a number such that the sum of the cubes of its digits is equal to the number itself. 371 is an Armstrong number since 33 + 73 + 13 = 371. 
+// Note: Return true if it is an Armstrong number else return false.
+// Examples
+// Input: n = 153
+// Output: true
+// Explanation: 153 is an Armstrong number since 13 + 53 + 33 = 153. 
+// Input: n = 372
+// Output: false
+// Explanation: 372 is not an Armstrong number since 33 + 73 + 23 = 378. 
+// Input: n = 100
+// Output: false
+// Explanation: 100 is not an Armstrong number since 13 + 03 + 03 = 1. 
+// Constraints:
+// 100 ≤ n <1000 
+// User function Template for Java
+class Solution {
+
+    static boolean armstrongNumber(int n) {
+        // code here
+        int temp = n;
+        int sum = 0;
+        while (n > 0) {
+            int dig = n % 10;
+            sum = sum + dig * dig * dig;
+            n /= 10;
+        }
+        return temp == sum;
+    }
+}
